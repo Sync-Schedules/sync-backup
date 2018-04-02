76953b7883f3d0f7a7ea6f8d4c879cd7b7bbad08
Pablo <pa.romerorgz@gmail.com>
2018-04-02 16:07:21 -0500

    master is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,6 +58,11 @@ const routes: Routes = [
     path: 'profile',
     component: ProfileComponent,
     canActivate:[AuthGuard]
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate:[AuthGuard]
   }
 ];
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,11 @@
-import { Injectable} from "@angular/core";
-import { Router, CanActivate} from "@angular/router";
-import { AuthService} from "../services/auth.service";
+import { Injectable } from "@angular/core";
+import { Router, CanActivate } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { MatSnackBar } from "@angular/material";
 
 @Injectable()
 export class AuthGuard implements CanActivate{
-  constructor(private as:AuthService, private r: Router){
+  constructor(private as:AuthService, private r: Router, private snack: MatSnackBar){
 
   }
 
@@ -13,6 +14,7 @@ export class AuthGuard implements CanActivate{
       return true;
     }else{
       this.r.navigate(['/login']);
+      this.snack.open('You must be logged in to see this', '', {duration:3000})
       return false;
     }
   }
