32a56d4182db547565138fa65e61a5e03496ecc6
Pablo <pa.romerorgz@gmail.com>
2018-04-02 11:39:42 -0500

    master is done

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var book = require('./routes/book');
-var app = express();
+const book = require('./routes/book');
+const app = express();
 
-var mongoose = require('mongoose');
-
+const mongoose = require('mongoose');
 
 
 app.use(logger('dev'));
